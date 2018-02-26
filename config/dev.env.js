45e36014fb4bb69f4031633b727c663c8c3b4a60
ellis-wu <ellis.w@inwinstack.com>
2018-02-26 17:52:19 +0800

    Update

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   SERVER_PROTOCOL: '"https"',
-  SERVER_HOST: '"0.0.0.0"',
+  SERVER_HOST: '"127.0.0.1"',
   SERVER_PORT: '"6443"',
   IKM_VERSION: '"v1alpha1"',
   IKM_API_GROUP: '"ikm.io"',
